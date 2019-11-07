bc9fb82a2edf0c670e187f123db22858c1e3a846
waj949 <wajdihamrouni1993@gmail.com>
2019-11-07 16:51:27 +0100

    fillThedivs

--- main.js
+++ main.js
@@ -9,4 +9,32 @@ function generateRgb(){
 var test = generateRgb()
 console.log(test);
 
-$('body').css('background', 'rgb' + test);
+//$('body').css('background', 'rgb' + test);
+function fillArrayOfColors(difficulity){
+	var acc = 0;
+	var arrayOfColors = [];
+	if(difficulity === 'easy'){
+		acc = 3;
+	}
+	else if (difficulity === 'medium') {
+		acc = 6;
+	} else {
+		acc = 9;
+	} 
+	for(i = 0; i < acc; i++){
+		arrayOfColors[i] = generateRgb();
+	}
+	console.log(arrayOfColors);
+    return arrayOfColors;
+}
+
+var arrayOfColors = fillArrayOfColors('hard');
+
+function fillTheDivs(arrayOfColors){
+	for(i = 0; i < arrayOfColors.length; i++){
+		$('.'+i).css('background', 'rgb'+ arrayOfColors[i])
+	}
+}
+
+fillTheDivs(arrayOfColors);
+
